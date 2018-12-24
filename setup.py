86d7e4714573592b31bb80346201d48421403c54
panicoenlaxbox <panicoenlaxbox@gmail.com>
2018-12-24 12:27:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pollite_greeter',
+    version='0.1',
+    packages=['pollite_greeter'],
+    url='http://github.com/panicoenlaxbox',
+    license='',
+    author='Sergio León',
+    author_email='panicoenlaxbox@gmail.com',
+    description='A pollite greeter'
+)
