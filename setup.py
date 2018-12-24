e4cf2abdd0e82b08165cdf2f31083f64e27e96c8
panicoenlaxbox <panicoenlaxbox@gmail.com>
2018-12-24 12:29:55 +0100

    Updated license and url

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='pollite_greeter',
     version='0.1',
     packages=['pollite_greeter'],
-    url='http://github.com/panicoenlaxbox',
-    license='',
+    url='https://github.com/panicoenlaxbox/pollite_greeter',
+    license='MIT',
     author='Sergio León',
     author_email='panicoenlaxbox@gmail.com',
     description='A pollite greeter'
